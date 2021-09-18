bff408c13c048cdc9032432991bffa2a9e9eb8c7
WLS <77196153+WLS-SD@users.noreply.github.com>
2021-09-18 02:06:38 +0000

    Add files via upload

---
+++ bash23.sh
@@ -0,0 +1,23 @@
+
+GREEN="\e[92m"
+
+printf "$GREEN"
+
+echo "
+|========================================================|
+|DEVELOPER : WLS-SD                                      |   
+|GitHub : github.com/WLS-SD                              | 
+|========================================================|
+| United States                          China           |
+| United Kingdom                         Israel          |
+| Netherlands                            Soudi Arabia    |
+| Japan                                  Thailand        |
+| Brazil                                 Ukraine         |
+| Turkey                                 Serbia          |
+| Iran                                   Germany         |
+| Spain                                  Portugal        |
+| Canada                                 Malaysia        |
+| Finland                                France          |
+|========================================================|
+
+"
