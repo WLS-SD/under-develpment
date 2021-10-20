892b9f9bb136bdf8cfbc4a1f8bea14910c01075f
Tech Hacked <77196153+WLS-SD@users.noreply.github.com>
2021-10-20 17:17:25 +0545

    Update bash23.sh

--- bash23.sh
+++ bash23.sh
@@ -15,9 +15,14 @@ echo "
 | [9] Twitter                            [10] Amazon     |
 | [11] GitHub                            [12] PUBG       |
 | [13] Jio Router                        [14] Wi-Fi      |
-| [15]                                   Portugal        |
-| Canada                                 Malaysia        |
-| Finland                                France          |
+| [15] Apple                             [16] Gmail      |
+| [17] Linkedin                          [18] Shopify    |
+| [19] WordPress                         [20] Netflix    |
+| [21] Clash Of Clans                    [22] SnapChat   |
+| [23] Xbox                              [24] Ola        |
+| [25] Social Club                       [26] Yahoo      |
+| [27] OutLook                           [28] Steam      |
+| [29] YouTube                                           |
 |========================================================|
 
 "
