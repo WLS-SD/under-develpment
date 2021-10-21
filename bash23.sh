297bfca5b012bd6f157693c0431409b76516799e
Tech Hacked <77196153+WLS-SD@users.noreply.github.com>
2021-10-21 07:28:54 +0545

    Update bash23.sh

--- bash23.sh
+++ bash23.sh
@@ -4,26 +4,26 @@ GREEN="\e[92m"
 printf "$GREEN"
 
 echo "
-|========================================================|
-|DEVELOPER : WLS-SD                                      |   
-|GitHub : github.com/WLS-SD                              | 
-|========================================================|
-| [1] Facebook                           [2] Instagram   |
-| [3] PayPal                             [4] Airtel      |
-| [5] Telegram                           [6] Ajio        |
-| [7] Free Fire                          [8] PlayStation |
-| [9] Twitter                            [10] Amazon     |
-| [11] GitHub                            [12] PUBG       |
-| [13] Jio Router                        [14] Wi-Fi      |
-| [15] Apple                             [16] Gmail      |
-| [17] Linkedin                          [18] Google     |
-| [19] WordPress                         [20] Netflix    |
-| [21] Clash Of Clans                    [22] SnapChat   |
-| [23] Xbox                              [24] Ola        |
-| [25] Social Club                       [26] Yahoo      |
-| [27] OutLook                           [28] Steam      |
-| [29] YouTube                           [30] Shopify    |
-| [30] About Developer                                   |
-|========================================================|
+|=========================================================|
+|DEVELOPER : WLS-SD          |                            |   
+|GitHub : github.com/WLS-SD  |                            | 
+|=========================================================|
+| [1] Facebook               |           [2] Instagram    |
+| [3] PayPal                 |            [4] Airtel      |
+| [5] Telegram               |            [6] Ajio        |
+| [7] Free Fire              |            [8] PlayStation |
+| [9] Twitter                |            [10] Amazon     |
+| [11] GitHub                |             [12] PUBG      |
+| [13] Jio Router            |            [14] Wi-Fi      |
+| [15] Apple                 |           [16] Gmail       |
+| [17] Linkedin              |            [18] Google     |
+| [19] WordPress             |            [20] Netflix    |
+| [21] Clash Of Clans        |            [22] SnapChat   |
+| [23] Xbox                  |            [24] Ola        |
+| [25] Social Club           |            [26] Yahoo      |
+| [27] OutLook               |            [28] Steam      |
+| [29] YouTube               |            [30] Shopify    |
+| [30] About Developer       |                            |
+|=========================================================|
 
 "
